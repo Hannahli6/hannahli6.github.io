4691ff231b5298ab08f536ac064356bc19ca9f0f
Hannah Li <hli149@students.cs.ubc.ca>
2025-07-16 19:25:37 -0700

    fixed fade in animation margin problem

--- src/components/FadeInAnimation/FadeInAnimation.jsx
+++ src/components/FadeInAnimation/FadeInAnimation.jsx
@@ -4,20 +4,21 @@ import "./fadeInAnimation.css";
 
 const FadeInAnimation = ({ children, direction }) => {
   direction = direction || "left";
-  console.log("FadeInAnimation direction:", direction);
   const { ref, inView } = useInView({
     threshold: 0.1,
     triggerOnce: false,
   });
 
   return (
-    <div
-      ref={ref}
-      className={`fade-in-animation ${inView ? "is-visible"  : ""} ${direction}`}
-    >
-      {children}
-      {console.log("FadeInAnimation inView:", inView)}
+    <div className="fade-in-wrapper">
+      <div
+        ref={ref}
+        className={`fade-in-animation ${inView ? "is-visible"  : ""} ${direction}`}
+      >
+        {children}
+      </div>
     </div>
+    
   );
 };
 
